7d9677b1e124093410fa9117877d9ac62151f5fa
krm111 <32006021+krm111@users.noreply.github.com>
2017-09-15 23:42:45 -0500

    (Program)Calculator

---
+++ Strategy/Calculator.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class Calculator {
+    private Map<String, StrategyOperation> operations = new HashMap<>();
+    
+    public void register(String op, StrategyOperation strategy) {
+        operations.put(op, strategy);
+    }
+    
+    public int execute(String op, int a, int b) {
+        StrategyOperation strategy = operations.get(op);
+        return strategy.operate(a, b);
+    }
+}
